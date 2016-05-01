93298f3fc1754b399f64da79cddf2385598745d7
ayaseeri@126.com <mymisaka@gmail.com>
2016-05-01 17:26:26 +0800

    no message

--- Lesson4_MVC/Lesson2_draw5Star/Demo Resources/RWTSquare.m
+++ Lesson4_MVC/Lesson2_draw5Star/Demo Resources/RWTSquare.m
@@ -10,9 +10,9 @@
 #import "RWTBaseEffect.h"
 #import "RWTVertex.h"
 const static RWTVertex vertices[] = {
-    {{1, -1, 0}, {1, 0, 0, 1}}, // V0
-    {{1, 1, 0}, {0, 1, 0, 1}}, // V1
-    {{-1, 1, 0}, {0, 0, 1, 1}}, // V2
+    {{1, -1, 0},  {1, 0, 0, 1}}, // V0
+    {{1, 1, 0},   {0, 1, 0, 1}}, // V1
+    {{-1, 1, 0},  {0, 0, 1, 1}}, // V2
     {{-1, -1, 0}, {0, 0, 0, 0}} // V3
 };
 
@@ -27,8 +27,8 @@ const static GLubyte indices[] = {
 - (instancetype)initWithShader:(RWTBaseEffect *)shader
 {
     if ((self = [super initWithName:"square" shader:shader vertices:(RWTVertex *)vertices vertexCount:sizeof(vertices)/sizeof(vertices[0]) inidices:(GLubyte *)indices indexCount:sizeof(indices)/sizeof(indices[0])])) {
-    
-}
+        ;
+    }
     return self;
 }
 @end
